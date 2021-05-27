36873ffc89a301d771f8a81f82e6445e6d7c945a
אורי בן-ארצי <oribena@post.bgu.ac.il>
2021-05-27 16:29:34 +0300

    permission

--- project/routes/league.js
+++ project/routes/league.js
@@ -90,4 +90,13 @@ router.post("/addReferee", async (req, res, next) => {
   }
 });
 
+router.post("/setPermission", async (req, res, next) => {
+  try {
+    await league_utils.setPermission(req.body.user_id, eq.body.permission);
+    res.status(201).send("Referee updated");
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
