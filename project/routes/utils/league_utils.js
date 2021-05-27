4956f57f8299c3066e62875769537c79236b46e4
אורי בן-ארצי <oribena@post.bgu.ac.il>
2021-05-27 17:49:02 +0300

    27.5

--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -4,101 +4,104 @@ const teams_utils = require("./teams_utils");
 const LEAGUE_ID = 271;
 
 async function getLeagueDetails() {
-    const league = await axios.get(
-        `https://soccer.sportmonks.com/api/v2.0/leagues/${LEAGUE_ID}`, {
-            params: {
-                include: "season",
-                api_token: process.env.api_token,
-            },
-        }
-    );
-    const stage = await axios.get(
-        `https://soccer.sportmonks.com/api/v2.0/stages/${league.data.data.current_stage_id}`, {
-            params: {
-                api_token: process.env.api_token,
-            },
-        }
-    );
-    return {
-        league_name: league.data.data.name,
-        current_season_name: league.data.data.season.data.name,
-        current_stage_name: stage.data.data.name,
-    };
+  const league = await axios.get(
+    `https://soccer.sportmonks.com/api/v2.0/leagues/${LEAGUE_ID}`,
+    {
+      params: {
+        include: "season",
+        api_token: process.env.api_token,
+      },
+    }
+  );
+  const stage = await axios.get(
+    `https://soccer.sportmonks.com/api/v2.0/stages/${league.data.data.current_stage_id}`,
+    {
+      params: {
+        api_token: process.env.api_token,
+      },
+    }
+  );
+  return {
+    league_name: league.data.data.name,
+    current_season_name: league.data.data.season.data.name,
+    current_stage_name: stage.data.data.name,
+  };
 }
 
 async function addMatch(date, time, hometeam, awayteam, stadium) {
-    await DButils.execQuery(
-        `INSERT INTO dbo.matches (date, time, hometeam, awayteam, stadium) VALUES ('${date}','${time}','${hometeam}','${awayteam}', '${stadium}')`
-    );
+  await DButils.execQuery(
+    `INSERT INTO dbo.matches (date, time, hometeam, awayteam, stadium) VALUES ('${date}','${time}','${hometeam}','${awayteam}', '${stadium}')`
+  );
 }
 
 async function addEvent(match_id, date, time, gamemin, event) {
-    await DButils.execQuery(
-        `INSERT INTO dbo.eventbook (match_id, date, time, gamemin, event) VALUES ('${match_id}','${date}','${time}','${gamemin}','${event}')`
-    );
+  await DButils.execQuery(
+    `INSERT INTO dbo.eventbook (match_id, date, time, gamemin, event) VALUES ('${match_id}','${date}','${time}','${gamemin}','${event}')`
+  );
 }
 
 async function addResult(match_id, result) {
-    await DButils.execQuery(
-        `UPDATE dbo.matches SET result = '${result}' WHERE match_id = '${match_id}'`
-    );
+  await DButils.execQuery(
+    `UPDATE dbo.matches SET result = '${result}' WHERE match_id = '${match_id}'`
+  );
 }
 
 async function addReferee(referee_id, match_id) {
-    await DButils.execQuery(
-        `UPDATE dbo.matches SET referee = '${referee_id}' WHERE match_id = '${match_id}'`
-    );
+  await DButils.execQuery(
+    `UPDATE dbo.matches SET referee = '${referee_id}' WHERE match_id = '${match_id}'`
+  );
 }
 
 async function setPermission(user_id, permission) {
-    await DButils.execQuery(
-        `UPDATE dbo.users SET permission = '${permission}' WHERE user_id = '${user_id}'`
-    );
+  await DButils.execQuery(
+    `UPDATE dbo.users SET permission = '${permission}' WHERE user_id = '${user_id}'`
+  );
 }
 
 function create_league(league_name, league_teams, league_policy, team_assign) {
-    return {
-        id: Math.floor(Math.random() * 400) + 1,
-        name: league_name,
-        teams: league_teams,
-        policy: league_policy,
-        team_assign: team_assign,
-    }
-
+  return {
+    id: Math.floor(Math.random() * 400) + 1,
+    name: league_name,
+    teams: league_teams,
+    policy: league_policy,
+    team_assign: team_assign,
+  };
 }
 
 async function assignMatches(teams_ids, policy) {
-    let teams_names = [];
-    teams_ids.forEach((id) => {
-        let name = teams_utils.getTeamName(id);
-        teams_names.push(name);
-    });
-    let assign = [];
-    // Each pair of teams will play against each other only once.
-    if (policy == 1) {
-        for (let i = 0; i < teams_names.length - 1; i++) {
-            for (let j = i + 1; j < teams_names.length; j++) {
-                let venue = teams_utils.getTeamVenue(teams_names[i]);
-                assign.push(teams_names[i] + ' vs. ' + teams_names[j] + ' at ' + venue);
-            }
-        }
+  let teams_names = [];
+  teams_ids.forEach((id) => {
+    let name = teams_utils.getTeamName(id);
+    teams_names.push(name);
+  });
+  let assign = [];
+  // Each pair of teams will play against each other only once.
+  if (policy == 1) {
+    for (let i = 0; i < teams_names.length - 1; i++) {
+      for (let j = i + 1; j < teams_names.length; j++) {
+        let venue = teams_utils.getTeamVenue(teams_names[i]);
+        assign.push(teams_names[i] + " vs. " + teams_names[j] + " at " + venue);
+      }
     }
-    // Each pair of teams will play twice, each time on the home field of one of the teams.
-    else if (policy == 2) {
-        for (let i = 0; i < teams_names.length - 1; i++) {
-            for (let j = i + 1; j < teams_names.length; j++) {
-                let venue = teams_utils.getTeamVenue(teams_names[i]);
-                let venue2 = teams_utils.getTeamVenue(teams_names[j]);
-                assign.push(teams_names[i] + ' vs. ' + teams_names[j] + ' at ' + venue);
-                assign.push(teams_names[j] + ' vs. ' + teams_names[i] + ' at ' + venue2);
-            }
-        }
+  }
+  // Each pair of teams will play twice, each time on the home field of one of the teams.
+  else if (policy == 2) {
+    for (let i = 0; i < teams_names.length - 1; i++) {
+      for (let j = i + 1; j < teams_names.length; j++) {
+        let venue = teams_utils.getTeamVenue(teams_names[i]);
+        let venue2 = teams_utils.getTeamVenue(teams_names[j]);
+        assign.push(teams_names[i] + " vs. " + teams_names[j] + " at " + venue);
+        assign.push(
+          teams_names[j] + " vs. " + teams_names[i] + " at " + venue2
+        );
+      }
     }
-    return assign;
+  }
+  return assign;
 }
 
 async function createNewLeague(league_name) {
-    DButils.createLeague(league_name);
+  DButils.createLeague(league_name);
 }
 
 exports.getLeagueDetails = getLeagueDetails;
