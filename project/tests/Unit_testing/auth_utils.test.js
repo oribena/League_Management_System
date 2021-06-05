5d70d5e4735d0be4192beb3a84b38580e8e20c01
noaakl <62835962+noaakl@users.noreply.github.com>
2021-06-05 21:52:17 +0300

    test

--- project/tests/Unit_testing/auth_utils.test.js
+++ project/tests/Unit_testing/auth_utils.test.js
@@ -1,6 +1,21 @@
+const DButils = require("../routes/utils/DButils");
+const auth_utils = require("./utils/auth_utils");
+
 test('register', () => {
+    // const user = {
+    //     {
+
+    //     }
+
+    const expectedUser = await DButils.execQuery(
+        `SELECT username, firstname, lastname, country, password, email, profilePic FROM dbo.users WERE `
+        // ( VALUES ('${username}','${firstname}','${lastname}','${country}', '${password}','${email}','${profilePic}')`
+    );
+    console.log(expectedUser)
+    // expect(auth_utils.register)
 
 });
+
 test('login', () => {
 
 });
