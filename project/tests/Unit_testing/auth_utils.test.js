3a091e113708d3bdcb9f270c6f19fee50c35486f
noaakl <62835962+noaakl@users.noreply.github.com>
2021-06-06 19:59:50 +0300

    test

--- project/tests/Unit_testing/auth_utils.test.js
+++ project/tests/Unit_testing/auth_utils.test.js
@@ -1,24 +1,142 @@
-const DButils = require('../../routes/utils/DButils');
+// const DButils = require('../../routes/utils/DButils');
 const auth_utils = require('../../routes/utils/auth_utils');
+const bcrypt = require("bcryptjs");
 
-test('register', async () => {
-    // const user = {
-    //     {
+// jest.setTimeout(10000000000000000);
+// DButils.connectDB();
 
-    //     }
-    const userID = 1;
-
+test('positive test register', async () => {
+    const pass = 'noa12345'
+    const hashedPass = bcrypt.hashSync(pass, parseInt(process.env.bcrypt_saltRounds));
+    const user = [{
+        username: 'noak',
+        firstname: 'noa',
+        lastname: 'kila',
+        country: 'Israel',
+        password: `${hashedPass}`,
+        email: 'nk@post.bgu.ac.il',
+        profilePic: 'https://cloudinary.com/NK'
+    }]
+    register(
+        user.username,
+        user.firstname,
+        user.lastname,
+        user.country,
+        user.password,
+        user.email,
+        user.profilePic
+    )
     const expectedUser = await DButils.execQuery(
-        `SELECT username, firstname, lastname, country, password, email, profilePic FROM dbo.users WERE user_id = userID`
-        // ( VALUES ('${username}','${firstname}','${lastname}','${country}', '${password}','${email}','${profilePic}')`
+        `SELECT username, password, firstname, lastname, country, email, profilePic FROM dbo.users WHERE username = '${user.username}'`
     );
-    console.log(expectedUser)
-    // expect(auth_utils.register)
+    expect(expectedUser).toEqual(user);
 
 });
 
-test('login', () => {
-
+test('negative test register username exists', async () => {
+    const user = [{
+        username: 'jlo',
+        firstname: 'noa',
+        lastname: 'kila',
+        country: 'Israel',
+        password: 'noa12345',
+        email: 'nk@post.bgu.ac.il',
+        profilePic: 'https://cloudinary.com/NK'
+    }]
+    expect(await auth_utils.register(
+        user.username,
+        user.firstname,
+        user.lastname,
+        user.country,
+        user.password,
+        user.email,
+        user.profilePic
+    )).toThrowError(ErrorMessageToken);
 });
 
-DButils.disconnectDB()
+
+test('positive test login', async () => {
+    const username = "jlo";
+    const loginResult = await auth_utils.login(username);
+    console.log(loginResult);
+    expect(loginResult.username).toEqual(username);
+    expect(loginResult.password).toBeTruthy();
+});
+
+test('negative test login username doesnt exist', async () => {
+    const username = "noaa";
+    const loginResult = await auth_utils.login(username);
+    expect(loginResult).toBeFalsy();
+});
+
+DButils.disconnectDB();
+
+
+
+
+
+
+
+
+
+
+// test('positive test register', async () => {
+//     const pass = 'noa12345'
+//     const hashedPass = bcrypt.hashSync(pass, parseInt(process.env.bcrypt_saltRounds));
+//     const user = [{
+//         username: 'noak',
+//         firstname: 'noa',
+//         lastname: 'kila',
+//         country: 'Israel',
+//         password: `${hashedPass}`,
+//         email: 'nk@post.bgu.ac.il',
+//         profilePic: 'https://cloudinary.com/NK'
+//     }]
+//     register(
+//         user.username,
+//         user.firstname,
+//         user.lastname,
+//         user.country,
+//         user.password,
+//         user.email,
+//         user.profilePic
+//     )
+//     const expectedUser = await DButils.execQuery(
+//         `SELECT username, password, firstname, lastname, country, email, profilePic FROM dbo.users WHERE username = '${user.username}'`
+//     );
+//     expect(expectedUser).toEqual(user);
+
+// });
+
+// test('negative test register username exists', async () => {
+//     const pass = 'noa12345'
+//     const hashedPass = bcrypt.hashSync(pass, parseInt(process.env.bcrypt_saltRounds));
+//     const user = [{
+//         username: 'jlo',
+//         firstname: 'noa',
+//         lastname: 'kila',
+//         country: 'Israel',
+//         password: `${hashedPass}`,
+//         email: 'nk@post.bgu.ac.il',
+//         profilePic: 'https://cloudinary.com/NK'
+//     }]
+//     await auth_utils.register(
+//         user.username,
+//         user.firstname,
+//         user.lastname,
+//         user.country,
+//         user.password,
+//         user.email,
+//         user.profilePic
+//     );
+//     expect(await auth_utils.register(
+//         user.username,
+//         user.firstname,
+//         user.lastname,
+//         user.country,
+//         user.password,
+//         user.email,
+//         user.profilePic
+//     )).toThrow();
+
+// });
--- project/tests/Unit_testing/team_utils.test.js
+++ project/tests/Unit_testing/team_utils.test.js
@@ -1,11 +1,33 @@
-const DButils = require('../../routes/utils/DButils');
+// const DButils = require('../../routes/utils/DButils');
+const teams_utils = require('../../routes/utils/teams_utils');
 
-test('getTeamName', () => {
+
+test('positive test getTeamName', async () => {
+    const teamId = 32223;
+    const teamName = await teams_utils.getTeamName(teamId);
+    expect(teamName).toBeTruthy();
 
 });
 
-test('getTeamVenue', () => {
+test('negative test getTeamName id doesnt exist', async () => {
+    const teamId = 1;
+    const teamName = await teams_utils.getTeamName(teamId);
+    expect(teamName).toBeFalsy();
 
 });
 
-DButils.disconnectDB()
+test('positive test getTeamVenue', async () => {
+    const teamName = 32223;
+    const teamVenue = await teams_utils.getTeamVenue(teamName);
+    expect(teamVenue).toBeTruthy();
+
+});
+
+test('negative test getTeamVenue name doesnt exist', async () => {
+    const teamName = 32223;
+    const teamVenue = await teams_utils.getTeamVenue(teamName);
+    expect(teamVenue).toBeTruthy();
+
+});
+
+// DButils.disconnectDB()
